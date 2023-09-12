6d1e8481621a51ad38a4f1d74e05a07d7154e686
StefanoM98 <Utente@PC-STEFANO>
2023-09-12 16:19:11 +0200

    Esercizio finito

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    public static class CalcoliHelper
+    {
+
+        public static double SommaDueNumeri(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+
+        public static double DifferenzaDueNumeri(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public static double MoltiplicaDueNumeri(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double ValoreAssoluto(double num1)
+        {
+            if (num1 < 0)
+            {
+                return num1 * -1;
+            }
+            else
+            {
+                return num1;
+            }
+        }
+
+        public static double Minimo(double num1, double num2)
+        {
+            if (num1 < num2)
+            {
+                return num1; 
+            } 
+            else
+            {
+                return num2;
+            }
+        }
+
+        public static double Massimo(double num1, double num2)
+        {
+            if (num1 > num2)
+            {
+                return num1;
+            }
+            else
+            {
+                return num2;
+            }
+        }
+
+    }
+}
--- csharp-calcolatrice/Program.cs
+++ csharp-calcolatrice/Program.cs
@@ -4,7 +4,87 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Benvenuto nella calcolatrice pazza");
+
+            int num1 = 3;
+            int num2 = 7;
+
+            double numd1 = 3.73924;
+            double numd2 = 5.39;
+
+
+
+
+            //Somma
+            Console.Write("Il risultato è: ");
+
+            Console.Write(CalcoliHelper.SommaDueNumeri(num1, num2));
+
+            Console.WriteLine();
+
+            Console.Write("Il risultato è: ");
+
+            Console.WriteLine(CalcoliHelper.SommaDueNumeri(numd1, numd2));
+
+
+            //Differenza
+            Console.Write("Il risultato è: ");
+
+            Console.Write(CalcoliHelper.DifferenzaDueNumeri(num1, num2));
+
+            Console.WriteLine();
+
+            Console.Write("Il risultato è: ");
+
+            Console.Write(CalcoliHelper.DifferenzaDueNumeri(numd1, numd2));
+
+            //Moltiplicazione
+            Console.Write("Il risultato è: ");
+
+            Console.Write(CalcoliHelper.MoltiplicaDueNumeri(num1, num2));
+
+            Console.WriteLine();
+
+            Console.Write("Il risultato è: ");
+
+            Console.Write(CalcoliHelper.MoltiplicaDueNumeri(numd1, numd2));
+
+            //Valore Assoluto
+
+            Console.Write("Il risultato è: ");
+
+            Console.Write(CalcoliHelper.ValoreAssoluto(num1));
+
+            Console.WriteLine();
+
+            Console.Write("Il risultato è: ");
+
+            Console.Write(CalcoliHelper.ValoreAssoluto(numd1));
+
+            Console.WriteLine();
+
+            //Valore Minimo
+
+            Console.Write("Il risultato è: ");
+            Console.Write(CalcoliHelper.Minimo(num1, num2));
+
+            Console.WriteLine();
+
+            Console.Write("Il risultato è: ");
+            Console.Write(CalcoliHelper.Minimo(numd1, numd2));
+
+            //Valore Massimo
+
+            Console.WriteLine();
+
+            Console.Write("Il risultato è: ");
+            Console.Write(CalcoliHelper.Massimo(num1, num2));
+
+            Console.WriteLine();
+
+            Console.Write("Il risultato è: ");
+            Console.Write(CalcoliHelper.Massimo(numd1, numd2));
         }
+
     }
 }
